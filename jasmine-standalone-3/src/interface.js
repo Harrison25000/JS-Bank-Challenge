e3119c7fe364387d5ac4d12aa0965bf7da9f6db4
Oliver <ofcharrison@gmail.com>
2019-04-10 12:25:24 +0100

    website works, run open index.html

--- jasmine-standalone-3/src/interface.js
+++ jasmine-standalone-3/src/interface.js
@@ -6,7 +6,23 @@ console.log(document.getElementById('deposit'))
 var deposit = document.getElementById('deposit')
 console.log(deposit);
 
-$('#form').on('submit', function(){
-  console.log('submitted')
+$('#form').on('submit', function(event) {
+});
+
+
+document.getElementById("form").addEventListener("submit", function(event){
+    //Stops the form submitting.
+    event.preventDefault();
+
+    //Do the checks here.
+    console.log(document.getElementById('deposit'))
+    console.log(event)
+    var valueDeposit = Number(event.target[0].value)
+    var valueWithdraw = Number(event.target[2].value)
+    account.deposit(valueDeposit)
+    account.withdraw(valueWithdraw)
+    console.log(account.balance)
+    $('#balance').text(account.balance);
+    //Sends the form.
 });
 });
